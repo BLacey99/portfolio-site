711f936ececa2a5056f606d5f9130ab6ced4efdb
Feras <blake.m.lacey@gmail.comD>
2025-02-07 03:00:46 -0500

    again

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-site/dist',
+  base: 'https://blacey99.github.io/portfolio-site/',
 })
